734ad9324fee2d198c7af4c2e2d8602a091b5f85
camilo nunez <camilonunezz9812@gmail.com>
2025-06-12 09:38:24 -0500

    feat: WelcomeView implementation

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import ButtonComponent from '@/components/ButtonComponent.vue'
+import PikachuBackgroundIcon from '@/components/icons/PikachuBackgroundIcon.vue'
+</script>
+
+<template>
+  <main class="welcome">
+    <section class="card">
+      <div class="pikachu-img-container">
+        <pikachu-background-icon />
+      </div>
+      <h1 class="text-subtitle text-bold text-center">Welcome to Pokédex</h1>
+      <p class="text-body text-center">
+        The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the
+        Pokémon world.
+      </p>
+      <div class="btn-container">
+        <button-component label="Get started" />
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped lang="css">
+.welcome {
+  display: grid;
+  place-items: center;
+  width: 100%;
+  height: 100%;
+  & .pikachu-img-container {
+    height: 275px;
+  }
+  & .card {
+    max-width: 540px;
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-xl);
+    & .btn-container {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+    }
+  }
+}
+</style>
