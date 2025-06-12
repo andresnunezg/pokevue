c868dc74bba1ef10aa0543ebaf5d39c65470ffdf
camilo nunez <camilonunezz9812@gmail.com>
2025-06-12 09:36:00 -0500

    style: create pikachu from img fragments

---
+++ src/components/icons/PikachuBackgroundIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="pikachu-img">
+    <figure>
+      <img src="@/assets/images/Ellipse.png" alt="red-background" />
+    </figure>
+    <figure>
+      <img src="@/assets/images/Pikachu.png" alt="pikachu" />
+    </figure>
+  </div>
+</template>
+
+<style scoped lang="css">
+.pikachu-img {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  figure {
+    position: absolute;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  figure:first-child {
+    z-index: 1;
+    bottom: 0px;
+  }
+
+  figure:last-child {
+    z-index: 2;
+    bottom: -12px;
+    transform: translate(3%, 0%);
+  }
+}
+</style>
